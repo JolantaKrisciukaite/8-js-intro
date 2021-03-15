1638e62cf496ce9c4c3392d0cc82f5a401404cdc
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-03-15 11:47:36 +0200

    ternary

---
+++ js/05-ternary.js
@@ -0,0 +1,51 @@
+/*
+
+TERNARY OPERATOR: ? (klaustukas)
+
+palyginimas ? ka daryti jei tenkina logika : ka daryti jei logikos
+netenkina
+
+palyginimas? true : false
+
+'nestinti' - kodas kode
+
+*/
+
+
+const a = 5;
+const b = 7;
+
+let ats = '';
+
+if (a > b) {
+    ats = 'daugiau';
+} else {
+    ats = 'ne daugiau';
+}
+
+console.log(ats);
+
+
+// inline if'as
+
+
+const c = 3;
+const d = 5;
+
+const ats2 = c > d ? 'daugiau' : 'ne daugiau';
+
+
+console.log(ats2);
+
+
+console.log('------------');
+
+
+const pazymiai = [5, 10, 7]
+const suma = pazymiai[0] + pazymiai[1] + pazymiai[2];
+
+const vidurkis = suma / pazymiai.length;
+console.log(vidurkis);
+
+
+console.log('------------');
