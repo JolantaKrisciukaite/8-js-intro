aa045773d499a579d9502f3410f2c7e39dfbd17c
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-03-18 12:10:16 +0200

    Digits

--- js/15-digits.js
+++ js/15-digits.js
@@ -1,24 +1,30 @@
 function digits(skaicius) {
     // input validation
-if (typeof skaicius !== 'number') {
-    return 'EROR: turi buti skaiciaus tipas'
-}
+    if (typeof skaicius !== 'number') {
+        return 'ERROR: turi buti skaiciaus tipas';
+    }
+    if (!isFinite(skaicius)) {
+        return `ERROR: ${skaicius} nera normalus skaicius`;
+    }
 
-if (isNaN(skaicius)) {       // if ('' + skaicius === 'NaN') {
-    return 'EROR: NaN nera normalus skaicius'
-}
-
-if (!isFinite(skaicius)) {      
-    return `EROR: ${skaicius} NaN nera normalus skaicius`
-}
     // logic
-    const ilgis = skaicius;
-    
-    
-    // return result
-    return ilgis;
-}
+    const tekstinisSkaicius = '' + skaicius;
+    let skaitmenuKiekis = tekstinisSkaicius.length;
 
+    // jei skaicius turi desimtaine dali, tai sumaziname 1 vienetu
+    if (skaicius % 1 !== 0) {
+        skaitmenuKiekis--;
+    }
+
+    // jei skaicius yra neigiamas, tai sumaziname 1 vienetu
+    if (skaicius < 0) {
+        skaitmenuKiekis--;
+    }
+
+
+    // return result
+    return skaitmenuKiekis;
+}
 
 console.log(digits(true));
 console.log(digits('asd'));
@@ -30,12 +36,18 @@ console.log(digits(undefined));
 console.log(digits(null));
 console.log(digits([]));
 console.log(digits(''));
-console.log(digits([4655]));
+console.log(digits([4624]));
 console.log(digits(digits));
 
+console.log(digits(5), '->', 1);
+console.log(digits(781), '->', 3);
+console.log(digits(37060123456), '->', 11);
+console.log(digits(3.1415), '->', 5);
+console.log(digits(+3.1415), '->', 5);
+console.log(digits(-456), '->', 3);
+console.log(digits(-3.1415), '->', 5);
 
-
-// console.log(digits(5), '->', 1);
-// console.log(digits(781), '->', 3 );
-// console.log(digits(37060123456), '->', 11);
-
+console.log('Koks sprendimas norint rasti standartines skaiciaus israiskos "turimu" skaitmenu kieki?');
+console.log(digits(100000000000000000000000000000000000000000000000000), '->', 51);
+console.log(digits(100000000000000000000000000000), '->', 30);
+console.log(digits(0.00000000000000000000000000001), '->', 30);
